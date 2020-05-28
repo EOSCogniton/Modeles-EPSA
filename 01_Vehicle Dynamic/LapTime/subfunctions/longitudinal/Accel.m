6c26695e1537e84fcfc328c4ccdd00b66fb7c13f
Bob <bob.aubouin@gmail.com>
2020-05-28 17:45:06 +0200

    Rangement

--- 01_Vehicle Dynamic/LapTime/subfunctions/longitudinal/Accel.m
+++ 01_Vehicle Dynamic/LapTime/subfunctions/longitudinal/Accel.m
@@ -13,45 +13,42 @@
 %               - vectors of speed, time and distance
 %__Hypotheses__
     % Le pneu est indeformable
-    % Le coeff d'adherence des pneus est constant
+    % Le coeff d'adherence des pneus est constant ==> modèle MF simplifié
     % Le coeff de résistance au roulement est constant
     % Le glissement est considere nul
     % La voiture n'a pas de suspensions
     % Le transfert de charge est instantane
     % Les pertes dans la transmission sont proportionnelles au couple
     % Le temps de passage de rapport ne depend pas des rapports concernes
-    % Les rapport passent sans debrayer
+    % Les rapport passent sans debrayer ==> pas du tout vrai ça
 
 %% Paramètres simulation
+function [V_acc,Gx,time] = Accel(D_max,param_file)
 
+load(param_file)
+step = 0.01; %pas de calcul de la simu
 %__Init__
-
 dsim = 0; % distance parcourue
-v = Vi/3.6; % vitesse du vehicule
+v = 0; % vitesse du vehicule
 tsim = -step; % temps
-j = 0; % Numero du point de fonctionnement du moteur
-t_acc = [0]; % Temps
-V_acc = [v]; % Vitesse
-A = [0]; % Acceleration en g
-RPM_acc = [r_pat]; % Regime moteur
-C = [0]; % Couple
-d_acc = [0]; % Distance
-E = [1]; % Embrayage 1=debraye, 0=embraye
-Adh = [1]; % Risque de patinage des pneus
-Ch_ar = [rep*m_t*g];
+t_acc = 0; % Temps
+V_acc = v; % Vitesse
+Gx = 0; % Acceleration en g
+a=0;% Acceleration instantanée en g
+RPM_acc = r_pat; % Regime moteur
+C = 0; % Couple
+d_acc = 0; % Distance
+E = 1; % Embrayage 1=debraye, 0=embraye
+Adh = 1; % Risque de patinage des pneus
+Ch_ar = rep*m_t*g;
 
 %__preliminar calculs__
 
 n = t_pas/step; % Nombre de pas necessaire au passage de rapport
 u = n;
-% Rapport engagé optimal selon la vitesse du véhicule
-k = 6; % Rapport engagé
-for gear=1:5
-    if Vi < shift_Speed(6-gear)
-        k = 6-gear; % Rapport engage
-    end
-end
-Ke = k;
+
+Ke = 1;
+k = 1;
 
 b = coeff_roul*2/D_wheel; % Decalage du point d'appui (m)
 c_roul = m_t*g*b; % Resistance au roulement (N.m)
@@ -59,7 +56,7 @@ c_roul = m_t*g*b; % Resistance au roulement (N.m)
 J_trans = m_t*D_wheel^2/4; % Inertie equivalente des masses en translation (kg.m²)
 J_eq = J_trans + J_rot; % Inertie totale (kg.m²)
 %% Simulation
-while dsim < D_acc
+while dsim < D_max  
     tsim = tsim+step;
     t_acc = [t_acc tsim];
     Ke = [Ke k]; % Memoire du rapport engage
@@ -79,41 +76,42 @@ while dsim < D_acc
     if (u < n) || (r > r_rupteur) % Changement de rapport
         a=0; % Acceleration nulle si changement de rapport
         C = [C, 0]; % Memoire couple aux roues
-        A = [A,0]; % Memoire acceleration
+        Gx = [Gx,0]; % Memoire acceleration
         Adh = [Adh, 0]; % Pas de risque de patinage des pneus
         Ch_ar =[Ch_ar rep*m_t*g];
     else     % Pas de changement de rapport 
         F_deportance_essieu_arr = 1/2*rho*v^2*Cz*Cz_rep;
         F_trainee = 1/2*rho*v^2*Cx;
-        c_k = interp1(rmot,cmot,r)*pertes/(k_p*K(k)*k_f); % Couple a la roue au rapport engage (m.kg)
-        a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
-        C_ar = rep*m_t*g+m_t*a_ang*(D_wheel/2)*h/W + F_deportance_essieu_arr; % Charge sur l'essieu arrière avec prise en compte du transfert de masse
+        
+        c_k = interp1(rmot,cmot,r,'linear','extrap')*pertes/(k_p*K(k)*k_f); % Couple a la roue au rapport engage (N.m) 
+        C_ar = rep*m_t*g+m_t*a*h/W + F_deportance_essieu_arr; % Charge sur l'essieu arrière avec prise en compte du transfert de masse
+        
         if C_ar > m_t*g+ F_deportance_essieu_arr % Cas ou les roues avant se soulevent
             C_ar = m_t*g+ F_deportance_essieu_arr;
         end
         c_trans_ar = Long_tire_grip*C_ar*D_wheel/2; % Couple maximum transmissible
         if c_k > c_trans_ar % Risque de patinage des pneus
             Adh = [Adh,1];
-            c_k = c_trans_ar; % Prise en compte de la limite d'adherence des pneus
-            a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
-            C_ar = rep*m_t*g+m_t*a_ang*(D_wheel/2)*h/W + F_deportance_essieu_arr; % Charge sur l'essieu arrière avec prise en compte du transfert de masse
-            % non_influence_de_k_f
+            c_k = c_trans_ar; % Prise en compte de la limite d'adherence des pneus 
         else
             Adh = [Adh, 0];
-        end
-        C = [C, c_k]; % Memoire du couple aux roues
+        end    
         Ch_ar =[Ch_ar C_ar];
-        a = a_ang*D_wheel/2 - F_trainee / (m_t); % Acceleration du vehicule en m/s² non_influence_de_k_f
-        A = [A,a/g]; % Memoire acceleration en g
+        C = [C, c_k]; % Memoire du couple aux roues
+        a_ang = (c_k-c_roul-D_wheel/2*F_trainee)/J_eq; % Acceleration du vehicule en m/s²
+        a = a_ang*D_wheel/2; % Acceleration du vehicule en m/s²
+        Gx = [Gx,a]; % Memoire acceleration en m/s²
     end
+    
+    
     v = v + a*step; % Vitesse du vehicule
     V_acc = [V_acc v]; % Memoire de la vitesse
     dsim = dsim + v*step; % Distance parcourue
     d_acc = [d_acc, dsim]; % Memoire de la distance
-    r = v/(k_p*K(k)*k_f*D_wheel*3.14/60); % Calcul du regime moteur
+    r = v/(k_p*K(k)*k_f*D_wheel*pi/60); % Calcul du regime moteur
     % Changement de rapport
     % Changement au rupteur
-    if (r > r_rupteur) && (u >n) && (k<6) && (k<k_max)
+    if (r > r_rupteur) && (u >n) && (k<k_max)
         k = k+1;
         u=0;
         % Changement de rapport pour optimiser le couple
@@ -124,5 +122,17 @@ while dsim < D_acc
     u = u+1;
 end
 
+%remove double
+L = [];
+for i=2:length(V_acc)
+    if V_acc(i-1)==V_acc(i)
+        L = [L i];
+    end
+end
 
-V_acc = V_acc*3.6;
+idx = 1:length(V_acc);
+idx = setdiff(idx,L);
+V_acc = V_acc(idx);
+Gx = Gx(idx);
+time = tsim;
+end
--- 02_Models/01_Vehicle Dynamic/Lap_Time_Model/03_Track_Plot/ortho.m
+++
@@ -1,4 +0,0 @@
-function new_vector = ortho(vector)
-new_vector = [-vector(2) vector(1)]/norm(vector);
-end
-
